150c6be90995d4961f4dc259faf32f39238d4382
Tyler Gabriel <tygabriel7@gmail.com>
2019-09-09 21:09:39 -0400

    Started Flexbox
    Yuh

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -4,9 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatCardModule} from '@angular/material/card';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 import { AppRoutingModule } from './app-routing.module';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { HomeComponent } from './home/home.component'
@@ -20,9 +24,12 @@ import { HomeComponent } from './home/home.component'
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
 		MatCardModule,
     MatExpansionModule,
+    MatGridListModule,
 		MatSidenavModule,
+    MatToolbarModule,
 		AppRoutingModule
   ],
   providers: [],
