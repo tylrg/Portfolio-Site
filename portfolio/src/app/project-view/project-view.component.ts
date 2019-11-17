5d192b6231ca747d4753abf40bb14713c7c5fe66
Tyler Gabriel <tygabriel7@gmail.com>
2019-11-16 19:57:15 -0500

    Fixed project list issues
    need to pass in values to project card

--- portfolio/src/app/project-view/project-view.component.ts
+++ portfolio/src/app/project-view/project-view.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from '../classes/project';
 
+
+
+
 @Component({
   selector: 'app-project-view',
   templateUrl: './project-view.component.html',
   styleUrls: ['./project-view.component.scss']
 })
-export class ProjectViewComponent implements OnInit {
+export class ProjectViewComponent implements OnInit{
 
-  projectList: Array<Project>;
+  projectList = [
+  { "title": "Taco", "description": "lorem ipsum taco bell taco", "date": "December 20th, 2019", "image": "none", "link": "www.google.com" },
+  { "title": "Taco", "description": "lorem ipsum taco bell taco", "date": "December 20th, 2019", "image": "none", "link": "www.google.com" },
+  { "title": "Taco", "description": "lorem ipsum taco bell taco", "date": "December 20th, 2019", "image": "none", "link": "www.google.com" },
+  ]; 
+  //favorite: this.projectList[0];
+  
 
   constructor() { }
 
   ngOnInit() {
-    
+  }
+
+  fuck(){
+    console.log(this.projectList);
   }
 
 }
